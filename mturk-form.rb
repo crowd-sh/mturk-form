7dca1e44f21b03f956fedc6f2514422e11ee022d
Abhi Yerra <abhi@berkeley.edu>
2016-12-08 00:44:55 +0000

    Simplify

--- mturk-form.rb
+++ mturk-form.rb
@@ -1,12 +1,5 @@
 require 'json'
 require 'erb'
 
-
-
-def main
-  @form = JSON.parse(open(ARGV[0]).read)
-
-  puts ERB.new(open('form.html.erb').read).result
-end
-
-main
+@form = JSON.parse(open(ARGV[0]).read)
+puts ERB.new(open('form.html.erb').read).result
